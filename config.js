9ff645f6d0b04d0237f571112019d59d59fac453
Camilla Giuliani <camilla.giuliani@live.com>
2026-05-10 17:12:48 +0200

    new file for config variables

---
+++ config.js
@@ -0,0 +1,44 @@
+// variable to enable start screen
+var ENABLE_START_SCREEN = false; 
+
+// Object paths
+var modelPath_teapot = "./Objects/teapot.obj";
+var modelPath_table = "./Objects/table.obj";
+var modelPath_cat= "./Cat/cat.obj";
+var modelPath_dog = "./dog/dog.obj";
+var modelPath_ball = "./ball_color/ball.obj";
+
+// images paths
+var path_img_teapot="./Textures/teapot_tex_1.png";
+var path_img_table="./table_tex_512.jpg";
+var path_img_cat="./Cat/cat_diffuse.jpg";
+var path_img_wall="./Textures/wall_tex.jpg";
+var path_img_floor="./Textures/parquet_tex.jpg";
+var path_img_dog = "./dog/dog_diff.jpg";
+var path_img_skybox = "./skybox/skybox.jpg";
+var path_img_skybox_night = "./Cubemap/cubemap_sky_night.png";
+var path_img_painting = "./Textures/london.jpg";
+var path_img_cornice = "./Textures/blue_navy.jpg";
+var path_img_ball= "./ball_color/ball_diff.jpg";
+
+
+//texture variables
+var useTexture_teapot = false;
+var useTexture_table = false;
+
+var teapotTexture;
+var teapotBuffers;  
+var tableTexture;
+var tableBuffers;
+var catBuffers; 
+var catTexture;
+var wallTexture;
+var floorTexture;
+var dogBuffers;
+var dogTexture;
+var skyboxTexture;
+var paintingTexture;
+var corniceTexture;
+var ballTexture;
+
+
